9443013eb0b43eb5165de9f3a8454824ce1336ba
Sumit Singh <132432995+sumittt08@users.noreply.github.com>
2025-12-09 05:30:10 +0000

    day2

---
+++ Basic/day2.js
@@ -0,0 +1,37 @@
+//let a=23
+//const b = "abc"
+//var c=78
+//d=88
+//console.log(a);
+//console.log(b);
+//console.log(c);
+//console.log(d);
+
+/*let a ="abc"
+let b=""
+let c=null
+let d;
+let e=true
+let f=undefined;
+
+console.log(a);
+
+console.log(b);
+console.log(c);
+console.log(d);
+console.log(e);
+
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+console.log(typeof d);
+console.log(typeof f);
+*/
+let score="33abc";
+console.log(typeof score);
+console.log(typeof (score));
+
+let newscore=Number(score);
+console.log(typeof newscore);
+console.log(typeof (newscore));
+console.log(newscore);
